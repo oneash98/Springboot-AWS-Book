a190a1537d7fc576284db7dc4dc11fa8db2f260b
ONEASH <oneash082498@gmail.com>
2022-07-13 20:57:35 +0900

    CREATE Controller&ControllerTest

--- src/main/java/com/mysite/springboot/Application.java
+++ src/main/java/com/mysite/springboot/Application.java
@@ -1,10 +1,15 @@
 package com.mysite.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
